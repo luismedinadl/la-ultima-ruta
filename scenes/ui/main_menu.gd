28e1773bc7a78bf389f73e7dc2eafd03fbdd34bb
luismedinadl <medinadelira@gmail.com>
2026-09-19 21:29:41 -0600

    Menú principal

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _ready() -> void:
+	$PlayButton.pressed.connect(_on_play_button_pressed)
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
