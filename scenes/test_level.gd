d954fe05a85c270d143e9a59f4835d7c7ccf6437
luismedinadl <medinadelira@gmail.com>
2026-09-19 22:09:35 -0600

    Pantalla de victoria

--- scenes/test_level.gd
+++ scenes/test_level.gd
@@ -18,6 +18,9 @@ extends Node3D
 @onready var resume_button: Button = $HUD/PausePanel/ResumeButton
 @onready var restart_button: Button = $HUD/PausePanel/RestartButton
 @onready var menu_button: Button = $HUD/PausePanel/MenuButton
+@onready var victory_panel: Panel = $HUD/VictoryPanel
+@onready var play_again_button: Button = $HUD/VictoryPanel/PlayAgainButton
+@onready var victory_menu_button: Button = $HUD/VictoryPanel/VictoryMenuButton
 
 var has_package := false
 var delivery_complete := false
@@ -26,6 +29,7 @@ var delivery_complete := false
 func _ready() -> void:
 	get_tree().paused = false
 	pause_panel.process_mode = Node.PROCESS_MODE_ALWAYS
+	victory_panel.process_mode = Node.PROCESS_MODE_ALWAYS
 
 	camera.global_position = player.global_position + camera_offset
 	camera.look_at(player.global_position + Vector3(0.0, 2.0, 0.0))
@@ -36,6 +40,7 @@ func _ready() -> void:
 	game_over_label.hide()
 	delivery_complete_label.hide()
 	pause_panel.hide()
+	victory_panel.hide()
 
 	delivery_package.connect("collected", _on_package_collected)
 	delivery_zone.connect("player_entered", _on_delivery_zone_entered)
@@ -43,6 +48,8 @@ func _ready() -> void:
 	resume_button.pressed.connect(_resume_game)
 	restart_button.pressed.connect(_restart_game)
 	menu_button.pressed.connect(_go_to_main_menu)
+	play_again_button.pressed.connect(_play_again)
+	victory_menu_button.pressed.connect(_go_to_main_menu)
 
 	_update_objective()
 
@@ -114,7 +121,10 @@ func _on_delivery_zone_entered() -> void:
 	if has_package and not delivery_complete:
 		delivery_complete = true
 		print("Entrega completada.")
-		_update_objective()
+
+		delivery_complete_label.hide()
+		victory_panel.show()
+		get_tree().paused = true
 
 
 func _update_objective() -> void:
@@ -124,3 +134,7 @@ func _update_objective() -> void:
 		objective_label.text = "Objetivo: Lleva el paquete a la zona verde"
 	else:
 		objective_label.text = "Objetivo: Recoge el paquete"
+
+func _play_again() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
