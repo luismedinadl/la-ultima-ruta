bf7cc3971f43b3d06b3c609c6c3ef3fec34a8272
luismedinadl <medinadelira@gmail.com>
2026-09-19 22:25:21 -0600

    Sonido de botones

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,10 +1,14 @@
 extends Control
 
+@onready var button_sound: AudioStreamPlayer = $ButtonSound
+
 func _ready() -> void:
 	$PlayButton.pressed.connect(_on_play_button_pressed)
 	$QuitButton.pressed.connect(_on_quit_button_pressed)
 
 func _on_play_button_pressed() -> void:
+	button_sound.play()
+	await button_sound.finished
 	get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
 
 func _on_quit_button_pressed() -> void:
