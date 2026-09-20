f7b3cf439d40dae868d354927329332bd483ecc4
luismedinadl <medinadelira@gmail.com>
2026-09-19 21:32:55 -0600

    Opciones del menú principal

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready() -> void:
 	$PlayButton.pressed.connect(_on_play_button_pressed)
+	$QuitButton.pressed.connect(_on_quit_button_pressed)
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
