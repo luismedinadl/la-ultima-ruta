25847e5bbc6c3c41f7a3679b17109ca53750b91b
luismedinadl <medinadelira@gmail.com>
2026-09-19 20:35:22 -0600

    Entrega completada

--- scenes/test_level.gd
+++ scenes/test_level.gd
@@ -9,6 +9,7 @@ extends Node3D
 @onready var game_over_label: Label = $HUD/GameOverLabel
 @onready var wave_label: Label = $HUD/WaveLabel
 @onready var objective_label: Label = $HUD/ObjectiveLabel
+@onready var delivery_complete_label: Label = $HUD/DeliveryCompleteLabel
 @onready var enemy_spawner = $EnemySpawner
 @onready var delivery_package = $DeliveryPackage
 @onready var delivery_zone = $DeliveryZone
@@ -24,6 +25,7 @@ func _ready() -> void:
 	health_bar.max_value = player.max_health
 	health_bar.value = player.health
 	game_over_label.hide()
+	delivery_complete_label.hide()
 
 	delivery_package.connect("collected", _on_package_collected)
 	delivery_zone.connect("player_entered", _on_delivery_zone_entered)
@@ -46,6 +48,15 @@ func _process(delta: float) -> void:
 		enemy_spawner.current_wave,
 		enemy_spawner.get_child_count()
 	]
+
+	if delivery_complete:
+		delivery_complete_label.show()
+		game_over_label.hide()
+
+		if Input.is_key_pressed(KEY_R):
+			get_tree().reload_current_scene()
+
+		return
 
 	if player.is_dead:
 		game_over_label.show()
